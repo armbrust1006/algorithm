f9c818821792b1a2394e9a57fd76fe60146abf3b
geunhwan kim <kimguenhwan@naver.com>
2020-10-01 23:03:30 +0900

    Category
    Cow

---
+++ category01/src/category01/AlgorithmMain.java
@@ -0,0 +1,43 @@
+package category01;
+
+import java.util.Scanner;
+
+public class AlgorithmMain {
+
+	/**
+	 * 牛が選択したケージ番号取得
+	 * @param sc
+	 * @param N 
+	 * @return
+	 */
+	public static String[][] getCowChoiceRooms(Scanner sc, int N) {
+		String[][] result = new String[N][];
+		for (int i = 0; i < N; i++) {
+			String[] input = sc.nextLine().split("\\s");
+			result[i] = input;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] input = sc.nextLine().split("\\s");
+		// 牛数
+		Integer N = Integer.parseInt(input[0]);
+		// ケージ数
+		Integer M = Integer.parseInt(input[1]);
+
+		// 축사 선택 정보
+		String[][] choiceInfo = getCowChoiceRooms(sc, N);
+
+		// 축사 배정 정보
+		String[] cage = new String[M];
+		// 마릿수만큼 루프
+		for (int i = 0; i < N; i++) {
+			
+		}
+		System.out.println();
+
+	}
+
+}
